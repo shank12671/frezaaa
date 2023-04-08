5b310a542829fb41ff7dd6c10d7d353bb067c91f
shank12671 <126411629+shank12671@users.noreply.github.com>
2023-04-09 01:02:55 +0530

    Update freza.js

--- freza.js
+++ freza.js
@@ -1,44 +1,17 @@
-const heartContainer = document.getElementById("hearts-container");
-const heartCountInput = document.getElementById("heart-count");
-const heartSizeInput = document.getElementById("heart-size");
-const animationSpeedInput = document.getElementById("animation-speed");
-const promptButton = document.getElementById("prompt-button");
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function createHeart() {
-  const heart = document.createElement("div");
-  heart.classList.add("heart");
-  heart.style.left = Math.random() * heartContainer.clientWidth + "px";
-  heart.style.animationDuration = animationSpeedInput.value + "s";
-  heart.style.width = heartSizeInput.value + "px";
-  heart.style.height = heartSizeInput.value + "px";
-  heartContainer.appendChild(heart);
-  setTimeout(() => {
-    heart.remove();
-  }, parseInt(heart.style.animationDuration) * 1000);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function handlePrompt() {
-  const response = prompt("What would you like to say to Freza?");
-  if (response) {
-    alert(`You said: "${response}"`);
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
   }
+  slides[slideIndex-1].style.display = "block";  
 }
-
-heartCountInput.addEventListener("change", () => {
-  while (heartContainer.firstChild) {
-    heartContainer.removeChild(heartContainer.firstChild);
-  }
-  for (let i = 0; i < heartCountInput.value; i++) {
-    createHeart();
-  }
-});
-
-heartSizeInput.addEventListener("input", () => {
-  const hearts = heartContainer.querySelectorAll(".heart");
-  hearts.forEach((heart) => {
-    heart.style.width = heartSizeInput.value + "px";
-    heart.style.height = heartSizeInput.value + "px";
-  });
-});
-
-promptButton.addEventListener("click", handlePrompt);
